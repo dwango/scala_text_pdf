041c6d60b25eb76b2d226acaaf9040d55593a833
Kota Mizushima <mizukota@gmail.com>
2018-01-25 16:19:08 +0900

    git user.email user.name を変更 (#9)

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ if [[ "${TRAVIS_OS_NAME}" == "linux" && "${TRAVIS_BRANCH}" == "master" && "${TRA
   openssl aes-256-cbc -k "$SERVER_KEY" -in deploy_key.enc -d -a -out deploy.key
   cp deploy.key ~/.ssh/
   chmod 600 ~/.ssh/deploy.key
-  git config --global user.email "m@yyu.pw"
-  git config --global user.name "Yoshimura Yuu"
+  git config --global user.email "mizukota@gmail.com"
+  git config --global user.name "Kota Mizushima"
   git fetch origin gh-pages:gh-pages
   git stash -u
   git checkout gh-pages
