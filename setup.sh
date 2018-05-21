47e01556be2d5680c730b54bc8fbae3a5f96f725
Yoshimura Yuu <m@yyu.pw>
2018-05-21 19:28:12 +0900

    Change PDF build

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git submodule update --init
+
 cd ./scala_text
 ../sbt tut
 cd ..
@@ -14,12 +16,16 @@ do
     inkscape -z -D --file=`pwd`/img/${BASH_REMATCH[1]}.svg --export-pdf=`pwd`/${BASH_REMATCH[1]}.pdf --export-latex=`pwd`/${BASH_REMATCH[1]}.pdf_tex
   fi
 done
-mkdir target
+
+if [[ ! -d "./target" ]]; then
+  mkdir target
+fi
+
 for f in ./scala_text/gitbook/*.md
 do
   if [[ $f =~ \./scala_text/gitbook/(.*)\.md ]]; then
     cp $f ./target/
-    FILENAME="${BASH_REMATCH[1]}.md" pandoc -o "./target/${BASH_REMATCH[1]}.tex" -f markdown_github+footnotes+header_attributes-hard_line_breaks-intraword_underscores --latex-engine=lualatex --chapters --listings --filter ./filter.py $f
+    FILENAME="${BASH_REMATCH[1]}.md" pandoc -o "./target/${BASH_REMATCH[1]}.tex" -f markdown_github+footnotes+header_attributes-hard_line_breaks-intraword_underscores --pdf-engine=lualatex --top-level-division=chapter --listings --filter ./filter.py $f
   fi
 done
 
