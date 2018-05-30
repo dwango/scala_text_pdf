bc2570b198b7081247840a1394fdd744f0b84360
Yoshimura Yuu <yyu@mental.poker>
2018-05-30 12:23:04 +0900

    Fix deploy_pdf.sh

--- travis/deploy_pdf.sh
+++ travis/deploy_pdf.sh
@@ -4,7 +4,7 @@ set -e
 
 if [[ "${TRAVIS_OS_NAME}" == "linux" && "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
   echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
-  openssl aes-256-cbc -k "$SERVER_KEY" -in deploy_key.enc -d -a -out deploy.key
+  openssl aes-256-cbc -k "$SERVER_KEY" -in ./travis/deploy_key.enc -d -a -out deploy.key
   cp deploy.key ~/.ssh/
   chmod 600 ~/.ssh/deploy.key
   git config --global user.email "mizukota@gmail.com"
